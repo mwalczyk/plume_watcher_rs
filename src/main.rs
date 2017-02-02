c81392052b5c6483f8f11f02b8127f7770fc95ed
Michael Walczyk <michael.walczyk@obscuradigital.com>
2017-02-02 11:04:52 -0800

    Added config class with builder pattern.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::path::Path;
+use std::process;
 use std::process::Command;
 use std::env;
 
 /// If the user enters an invalid command, print some instructions.
 fn help_dialog() {
     println!("usage:
-    spectrum_watcher_rs executable_path");
+    spectrum_watcher_rs path/to/program.exe");
 }
 
 /// Launches a child process at the specified path from the
@@ -13,7 +14,6 @@ fn help_dialog() {
 fn launch_process(path: &str, current: Option<&str>) {
     print!("launching: {} from directory: {:?}", path, current);
 
-    // launch the child process
     let mut command = Command::new(Path::new(path));
 
     if let Some(c) = current {
@@ -26,34 +26,51 @@ fn launch_process(path: &str, current: Option<&str>) {
     }
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let path;
-    let current;
+static MAX_THREADS: i32 = 10;
 
-    // Remember: the first argument is always the executing process
-    match args.len() {
-        1 => {
-            println!("spectrum_watcher_rs requires at least one command line argument");
+struct Config {
+    program: String,
+    working: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
         }
-        2 => {
-            path = &args[1];
-            loop {
-                launch_process(&path[..], None);
-            }
-        }
-        3 => {
-            path = &args[1];
-            current = &args[2];
-            loop {
-                launch_process(&path[..], Some(&current[..]));
-            }
-        }
-        _ => {
-            println!("error: invalid command");
-            help_dialog();
+
+        let program = args[1].clone();
+        let working = args[2].clone();
+
+        Ok(Config {
+            program: program,
+            working: working,
+        })
+    }
+}
+
+fn run(config: Config) {
+    println!("starting program: {}", config.program);
+    println!("working directory: {}", config.working);
+
+    let mut command = Command::new(Path::new(&config.program));
+    command.current_dir(Path::new(&config.working));
+
+    loop {
+        if let Ok(mut child) = command.spawn() {
+            child.wait().expect("command wasn't running");
+            println!("child process {} exited successfully", child.id());
         }
     }
+}
 
+fn main() {
+    let args: Vec<String> = env::args().collect();
 
+    let config = Config::new(&args).unwrap_or_else(|e| {
+        println!("problem parsing command line arguments: {}", e);
+        process::exit(1);
+    });
+
+    run(config);
 }
